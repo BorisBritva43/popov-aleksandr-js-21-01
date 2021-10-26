88d46fe30d4cbadfe04311571fd63787b0776b6a
AlexandrSurfunoff <sanchos8_7@mail.ru>
2021-10-26 18:48:19 +0300

    ДЗ-12 (create-react-app)

---
+++ homeWork_12_create_react_app/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+import { Header } from './components/header/Header';
+import { Sidebar } from './components/sidebar/Sidebar';
+import { Comments } from './forms/comments/Comments';
+import { Footer } from './components/footer/Footer';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+
+        <div className="wrapper">
+
+          <Header />
+
+          <div className="main">
+
+            <div className="sidebar__wrapper">
+              <Sidebar />
+            </div>
+
+            <div className="content">
+              <Comments />
+            </div>
+
+          </div>
+
+          <Footer />
+
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ homeWork_12_create_react_app/src/forms/comments/Comments.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Comments.css';
+import { apiResponse } from '../../api-mock/api';
+import { Comment } from '../../components/comment/Comment';
+
+export class Comments extends React.Component {
+   render() {
+      return (
+         <div className="comments">
+            {apiResponse.status === 'ok' ?
+               apiResponse.result.map((elem, index) => <Comment name={elem.name} text={elem.text} key={index} />) :
+               'При загрузке произошла ошибка'
+            }
+         </div>
+      )
+   }
+}
